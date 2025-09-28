fea1dca06ba1f5becb0f5719c65e74b8e76018d4
MosVV <vv-mosolov@rambler.ru>
2025-09-28 13:48:58 +0400

    Реализовали запросы к jsonplaceholder API

--- src/main/java/com/merion/retrofit/Main.java
+++ src/main/java/com/merion/retrofit/Main.java
@@ -1,4 +1,46 @@
 package com.merion.retrofit;
 
+import com.merion.retrofit.request.PostCreateRequest;
+import com.merion.retrofit.request.PostUpdateRequest;
+import com.merion.retrofit.response.JsonPlaceholderService;
+import com.merion.retrofit.response.PostResponse;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
+    public  static  void  main(String[] arg) throws IOException {
+        System.out.println("Hello");
+
+        JsonPlaceholderApi api = JsonPlaceholderService.getInstance().api();
+        System.out.println("GET:/posts ---------");
+        Response<List<PostResponse>> postResponses =api.posts(null).execute();
+        System.out.println(postResponses.isSuccessful());
+        System.out.println(postResponses.code());
+        System.out.println(postResponses.headers());
+        List<PostResponse> post =postResponses.body();
+        System.out.println(post);
+
+        System.out.println("POST:/posts ---------");
+        PostCreateRequest createRequest=new PostCreateRequest();
+        createRequest.setBody("create");
+        createRequest.getTitle("create");
+        createRequest.getUserId(1);
+        PostResponse postResponseCreat =api.create(createRequest).execute().body();
+        System.out.println(postResponseCreat);
+
+        System.out.println("PUT:/posts ---------");
+        PostUpdateRequest updateRequest=new PostUpdateRequest();
+        updateRequest.setBody("update");
+        updateRequest.setTitle("update");
+        updateRequest.setUserId(1);
+        updateRequest.setId(1);
+        PostResponse postResponseUpdate =api.update(1,updateRequest).execute().body();
+        System.out.println(postResponseUpdate);
+
+        System.out.println("DELETE:/posts ---------");
+        Integer code=api.delete(1).execute().code();
+        System.out.println(code);
+    }
 }
--- src/main/java/com/merion/retrofit/request/PostCreateRequest.java
+++ src/main/java/com/merion/retrofit/request/PostCreateRequest.java
@@ -1,4 +1,33 @@
 package com.merion.retrofit.request;
 
 public class PostCreateRequest {
+    private Integer userId;
+
+    private String title;
+
+    private String body;
+
+    public Integer getUserId(int i) {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle(String create) {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
